a6a3a17ef23bfc2bbc4b182f8c98597142e4b939
montekaka <josh.chen.jia@gmail.com>
2016-02-04 13:21:17 -0800

    added list of racers for a race

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,8 @@ module ApplicationHelper
 	def format_hours secs
 		Time.at(secs).utc.strftime("%k:%M:%S") if secs
  	end
-	def 
-		format_minutes secs Time.at(secs).utc.strftime("%M:%S") if secs
+	def format_minutes secs
+		Time.at(secs).utc.strftime("%M:%S") if secs
 	end
 	def format_mph mph
 		mph.round(1) if mph 
