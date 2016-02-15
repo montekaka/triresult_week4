a4d56e2f77616c9a7f62d62fe6bcb5b2ac155548
montekaka <josh.chen.jia@gmail.com>
2016-02-14 22:49:15 -0800

    add resource

--- app/controllers/api/results_controller.rb
+++ app/controllers/api/results_controller.rb
@@ -6,14 +6,15 @@ module Api
 			if !request.accept || request.accept == "*/*"
 				render plain: "/api/races/#{params[:race_id]}/results"
 			else
+				@race=Race.find(params[:race_id])
+				@entrants=@race.entrants
 			end
 		end
 		def show
 			if !request.accept || request.accept == "*/*"
 				render plain: "/api/races/#{params[:race_id]}/results/#{params[:id]}"
 			else
-				set_result
-				#render action: :show
+				@result = Race.find(params[:race_id]).entrants.where(id: params[:id]).first
 				render :partial=>"result", :object=>@result
 			end			
 		end
@@ -21,8 +22,8 @@ module Api
 	  # PATCH/PUT /races/1
 	  # PATCH/PUT /races/1.json
 	  def update
-	  	set_result	  	
-	  	entrant=@result
+	  	#set_value
+	  	entrant = Race.find(params[:race_id]).entrants.where(id: params[:id]).first	  						
 	  	result=params[:result]
 	  	if result 
 	  		if result[:swim]
@@ -33,17 +34,25 @@ module Api
 	  			entrant.t1=entrant.race.race.t1
 	  			entrant.t1_secs = result[:t1].to_f
 	  		end
-	  		entrant.save
-	  	end
-
-	  	# fresh_when(@result)
-	  	# @result.update(result_params)
-	  	# result = Race.find(@result)
-	  	# render json: result, status: :ok
+	  		if result[:bike]
+	  			entrant.bike = entrant.race.race.bike
+	  			entrant.bike_secs = result[:bike].to_f
+	  		end
+	  		if result[:t2]
+	  			entrant.t2 = entrant.race.race.t2
+	  			entrant.t2_secs = result[:t2].to_f
+	  		end
+	  		if result[:run]
+	  			entrant.run = entrant.race.race.run
+	  			entrant.run_secs = result[:run].to_f
+	  		end
+	  	end	
+	  	entrant.save 
+	  	render json: entrant, status: :ok 	
 	  end
 
 		private
-		def set_result
+		def set_value
 			@result = Race.find(params[:race_id]).entrants.where(:id=>params[:id]).first
 		end	
     # Never trust parameters from the scary internet, only allow the white list through.
